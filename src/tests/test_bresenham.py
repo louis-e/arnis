f622385c2c4a97de61d68a39ffaedf7027ae3f92
Edward Weir <edwardweir13579@gmail.com>
2022-10-20 12:45:10 +1000

    make test parameters more readable

--- src/tests/test_bresenham.py
+++ src/tests/test_bresenham.py
@@ -1,27 +1,71 @@
+import collections
 import pytest
 
 from src.bresenham import bresenham
 
 
+TestBresenhamParameters = collections.namedtuple(
+    "TestBresenhamParameters", ["x1", "y1", "x2", "y2", "result"]
+)
+
+
 @pytest.mark.parametrize(
-    ("x1", "y1", "x2", "y2", "result"),
+    "parameters",
     (
-        (0, 0, 0, 0, ((0, 0),)),
-        (0, 0, 5, 0, ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0))),
-        (0, 0, -5, 0, ((0, 0), (-1, 0), (-2, 0), (-3, 0), (-4, 0), (-5, 0))),
-        (0, 0, 0, 5, ((0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5))),
-        (0, 0, 0, -5, ((0, 0), (0, -1), (0, -2), (0, -3), (0, -4), (0, -5))),
-        (0, 0, 2, 3, ((0, 0), (1, 1), (1, 2), (2, 3))),
-        (0, 0, -2, 3, ((0, 0), (-1, 1), (-1, 2), (-2, 3))),
-        (0, 0, 2, -3, ((0, 0), (1, -1), (1, -2), (2, -3))),
-        (0, 0, -2, -3, ((0, 0), (-1, -1), (-1, -2), (-2, -3))),
-        (-1, -3, 3, 3, ((-1, -3), (0, -2), (0, -1), (1, 0), (2, 1), (2, 2), (3, 3))),
-        (
-            0,
-            0,
-            11,
-            1,
-            (
+        TestBresenhamParameters(x1=0, y1=0, x2=0, y2=0, result=((0, 0),)),
+        TestBresenhamParameters(
+            x1=0,
+            y1=0,
+            x2=5,
+            y2=0,
+            result=((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0)),
+        ),
+        TestBresenhamParameters(
+            x1=0,
+            y1=0,
+            x2=-5,
+            y2=0,
+            result=((0, 0), (-1, 0), (-2, 0), (-3, 0), (-4, 0), (-5, 0)),
+        ),
+        TestBresenhamParameters(
+            x1=0,
+            y1=0,
+            x2=0,
+            y2=5,
+            result=((0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)),
+        ),
+        TestBresenhamParameters(
+            x1=0,
+            y1=0,
+            x2=0,
+            y2=-5,
+            result=((0, 0), (0, -1), (0, -2), (0, -3), (0, -4), (0, -5)),
+        ),
+        TestBresenhamParameters(
+            x1=0, y1=0, x2=2, y2=3, result=((0, 0), (1, 1), (1, 2), (2, 3))
+        ),
+        TestBresenhamParameters(
+            x1=0, y1=0, x2=-2, y2=3, result=((0, 0), (-1, 1), (-1, 2), (-2, 3))
+        ),
+        TestBresenhamParameters(
+            x1=0, y1=0, x2=2, y2=-3, result=((0, 0), (1, -1), (1, -2), (2, -3))
+        ),
+        TestBresenhamParameters(
+            x1=0, y1=0, x2=-2, y2=-3, result=((0, 0), (-1, -1), (-1, -2), (-2, -3))
+        ),
+        TestBresenhamParameters(
+            x1=-1,
+            y1=-3,
+            x2=3,
+            y2=3,
+            result=((-1, -3), (0, -2), (0, -1), (1, 0), (2, 1), (2, 2), (3, 3)),
+        ),
+        TestBresenhamParameters(
+            x1=0,
+            y1=0,
+            x2=11,
+            y2=1,
+            result=(
                 (0, 0),
                 (1, 0),
                 (2, 0),
@@ -38,9 +82,20 @@ from src.bresenham import bresenham
         ),
     ),
 )
-def test_bresenham(x1, y1, x2, y2, result):
-    assert tuple(bresenham(x1=x1, y1=y1, x2=x2, y2=y2)) == result
-    assert tuple(bresenham(x1=x2, y1=y2, x2=x1, y2=y1)) == tuple(reversed(result))
+def test_bresenham(parameters: TestBresenhamParameters):
+    assert (
+        tuple(
+            bresenham(
+                x1=parameters.x1, y1=parameters.y1, x2=parameters.x2, y2=parameters.y2
+            )
+        )
+        == parameters.result
+    )
+    assert tuple(
+        bresenham(
+            x1=parameters.x2, y1=parameters.y2, x2=parameters.x1, y2=parameters.y1
+        )
+    ) == tuple(reversed(parameters.result))
 
 
 def test_min_slope_uphill():
