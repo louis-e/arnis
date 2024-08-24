478ffb67094f4e79e581bf48014abae73cbaba24
louis-e <44675238+louis-e@users.noreply.github.com>
2024-08-24 16:37:06 +0200

    Initial branch setup

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.9
-RUN apt-get update && apt-get -y install git ffmpeg libsm6 libxext6
-RUN cd /home && mkdir /home/region && git clone https://github.com/louis-e/arnis.git
-WORKDIR /home/arnis
-RUN pip install -r requirements.txt
-ENTRYPOINT ["python", "arnis.py"]
---
+++ src/main.rs
@@ -0,0 +1,167 @@
+use std::{collections::HashMap, fs::File};
+use fastanvil::Region;
+use fastnbt::{Value, LongArray};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Chunk {
+    sections: Vec<Section>,
+    #[serde(flatten)]
+    other: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Section {
+    block_states: Blockstates,
+    #[serde(flatten)]
+    other: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Blockstates {
+    palette: Vec<PaletteItem>,
+    #[serde(flatten)]
+    other: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PaletteItem {
+    #[serde(rename = "Name")]
+    name: String,
+    #[serde(rename = "Properties")]
+    properties: Option<Value>,
+}
+
+struct WorldEditor {
+    region: Region<File>,
+    new_region: Region<File>,
+    chunks_to_modify: HashMap<(usize, usize), Vec<(String, i32, i32, i32)>>,
+}
+
+impl WorldEditor {
+    /// Initializes the WorldEditor with the input region path and output region path.
+    fn new(region_path: &str, out_path: &str) -> Self {
+        let region_file = File::open(region_path).unwrap();
+        let region = Region::from_stream(region_file).unwrap();
+
+        let out_file = File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(out_path)
+            .unwrap();
+
+        let new_region = Region::new(out_file).unwrap();
+
+        Self {
+            region,
+            new_region,
+            chunks_to_modify: HashMap::new(),
+        }
+    }
+
+    /// Sets a block of the specified type at the given coordinates.
+    fn set_block(&mut self, block_type: &str, x: i32, y: i32, z: i32) {
+        let chunk_x = (x >> 4) as usize;
+        let chunk_z = (z >> 4) as usize;
+        self.chunks_to_modify
+            .entry((chunk_x, chunk_z))
+            .or_default()
+            .push((block_type.to_string(), x, y, z));
+    }
+
+    /// Saves all changes made to the world by writing modified chunks to the output region file.
+    fn save(&mut self) {
+        for ((chunk_x, chunk_z), block_list) in self.chunks_to_modify.drain() {
+            if let Ok(Some(data)) = self.region.read_chunk(chunk_x, chunk_z) {
+                let mut chunk: Chunk = fastnbt::from_bytes(&data).unwrap();
+
+                for (block_type, x, y, z) in block_list {
+                    set_block_in_chunk(&mut chunk, &block_type, x, y, z);
+                }
+
+                let ser = fastnbt::to_bytes(&chunk).unwrap();
+                self.new_region.write_chunk(chunk_x, chunk_z, &ser).unwrap();
+            }
+        }
+    }
+}
+
+fn main() {
+    let region_path = "r.0.0.mca.template";
+    let out_path = "%MC_WORLD_PATH_HERE%/region/r.0.0.mca";
+
+    // Initialize the WorldEditor
+    let mut editor = WorldEditor::new(region_path, out_path);
+
+    // Set multiple blocks
+    editor.set_block("minecraft:bedrock", 8, -59, 5);
+    editor.set_block("minecraft:bedrock", 8, -60, 5);
+    editor.set_block("minecraft:bedrock", 6, -60, 3);
+
+    // Save the changes to the world
+    editor.save();
+}
+
+/// Modifies a chunk to set a block of the given type at the specified coordinates.
+///
+/// # Arguments
+///
+/// * `chunk` - Mutable reference to the chunk to be modified.
+/// * `block_type` - The block type to set.
+/// * `x`, `y`, `z` - The global coordinates of the block.
+fn set_block_in_chunk(chunk: &mut Chunk, block_type: &str, x: i32, y: i32, z: i32) {
+    let local_x = (x & 15) as usize;
+    let local_y = y as usize;
+    let local_z = (z & 15) as usize;
+
+    for section in chunk.sections.iter_mut() {
+        if let Some(Value::Byte(y_byte)) = section.other.get("Y") {
+            if *y_byte == (local_y >> 4) as i8 {
+                let palette = &mut section.block_states.palette;
+                let block_index = (local_y % 16 * 256 + local_z * 16 + local_x) as usize;
+
+                let palette_index = if let Some(index) = palette.iter().position(|item| item.name == block_type) {
+                    index as u32
+                } else {
+                    palette.push(PaletteItem {
+                        name: block_type.to_string(),
+                        properties: None,
+                    });
+                    (palette.len() - 1) as u32
+                };
+
+                if let Some(Value::LongArray(ref mut data)) = section.block_states.other.get_mut("data") {
+                    set_block_in_section(data, block_index, palette_index);
+                } else {
+                    let mut new_data = vec![0; 256];
+                    set_block_in_section(&mut new_data, block_index, palette_index);
+                    section.block_states.other.insert("data".to_string(), Value::LongArray(LongArray::new(new_data)));
+                }
+
+                println!("Set block {} at coordinates ({}, {}, {})", block_type, x, y, z);
+                break;
+            }
+        }
+    }
+}
+
+/// Modifies the block state data for a given block index within a section.
+///
+/// This function updates the block data array to set the block at the specified index
+/// to point to the correct palette entry.
+///
+/// # Arguments
+///
+/// * `data` - The block state data array to modify.
+/// * `block_index` - The index of the block to modify within the section.
+/// * `palette_index` - The index in the palette corresponding to the block type.
+fn set_block_in_section(data: &mut [i64], block_index: usize, palette_index: u32) {
+    let bits_per_block = 4.max(64 / data.len() as u32); // Calculate bits per block, minimum of 4
+    let mask = (1 << bits_per_block) - 1;
+    let long_index = block_index * bits_per_block as usize / 64;
+    let bit_index = (block_index * bits_per_block as usize) % 64;
+    data[long_index] &= !(mask << bit_index);
+    data[long_index] |= (palette_index as i64) << bit_index;
+}
