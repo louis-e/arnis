3efb8460c13404f1423c3fb3f6cd88afb0b351b2
louis-e <44675238+louis-e@users.noreply.github.com>
2025-01-12 16:02:56 +0100

    Readme enhancements and credits addition

--- gui-src/js/main.js
+++ gui-src/js/main.js
@@ -1,3 +1,5 @@
+import { licenseText } from './license.js';
+
 const { invoke } = window.__TAURI__.core;
 
 // Initialize elements and start the demo progress
@@ -247,6 +249,29 @@ function initSettings() {
   slider.addEventListener("input", () => {
     sliderValue.textContent = parseFloat(slider.value).toFixed(2);
   });
+
+
+  /// License and Credits
+  function openLicense() {
+    const licenseModal = document.getElementById("license-modal");
+    const licenseContent = document.getElementById("license-content");
+
+    // Render the license text as HTML
+    licenseContent.innerHTML = licenseText;
+
+    // Show the modal
+    licenseModal.style.display = "flex";
+    licenseModal.style.justifyContent = "center";
+    licenseModal.style.alignItems = "center";
+  }
+
+  function closeLicense() {
+    const licenseModal = document.getElementById("license-modal");
+    licenseModal.style.display = "none";
+  }
+
+  window.openLicense = openLicense;
+  window.closeLicense = closeLicense;
 }
 
 function initWorldPicker() {
@@ -306,6 +331,11 @@ function handleBboxInput() {
               // Input is valid; trigger the event
               const bboxText = `${lat1} ${lng1} ${lat2} ${lng2}`;
               window.dispatchEvent(new MessageEvent('message', { data: { bboxText } }));
+
+              // Show custom bbox on the map
+              let map_container = document.querySelector('.map-container');
+              map_container.setAttribute('src', `maps.html#${lat1},${lng1},${lat2},${lng2}`);
+              map_container.contentWindow.location.reload();
 
               // Update the info text
               bboxInfo.textContent = window.localization.custom_selection_confirmed;
