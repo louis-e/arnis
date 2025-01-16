louis-e/arnis

commit dce5f472f00b3cf65d746eb11e4da608272d58b3

Make the GUI partially work in a web browser
The lack of window.__TAURI__ object prevents most of the JS code
from running (this change stubs it which allows the Tauri GUI to
be tested in a web browser with a simple web server like PHP's one)

gui-src/js/main.js
@@ -1,6 +1,11 @@
 import { licenseText } from './license.js';
 
-const { invoke } = window.__TAURI__.core;
+if (window.__TAURI__) {
+  const { invoke } = window.__TAURI__.core;
+} else {
+  function dummyFunc() {}
+  window.__TAURI__ = { event: { listen: dummyFunc } };
+}
 
 // Initialize elements and start the demo progress
 window.addEventListener("DOMContentLoaded", async () => {

commit ede5367a84fd2a1452e159272ed3968d5f7acfbe

Use global scope for invoke definition
Fixes: dce5f47 ("Make the GUI partially work in a web browser")

gui-src/js/main.js
@@ -1,7 +1,7 @@
 import { licenseText } from './license.js';
 
 if (window.__TAURI__) {
-  const { invoke } = window.__TAURI__.core;
+  var { invoke } = window.__TAURI__.core;
 } else {
   function dummyFunc() {}
   window.__TAURI__ = { event: { listen: dummyFunc } };