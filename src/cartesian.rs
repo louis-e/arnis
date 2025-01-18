c573a02fd68242193283329548c7160f7534521e
louis-e <44675238+louis-e@users.noreply.github.com>
2025-01-18 23:52:08 +0100

    Optimized cartesian

--- src/cartesian.rs
+++ src/cartesian.rs
@@ -5,23 +5,8 @@ pub struct XZPoint {
 }
 
 impl XZPoint {
+    #[inline]
     pub fn new(x: i32, z: i32) -> Self {
         Self { x, z }
-    }
-}
-
-pub struct XYZPoint {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-}
-
-impl XYZPoint {
-    pub fn from_xz(xz: XZPoint, y: i32) -> Self {
-        Self {
-            x: xz.x,
-            y,
-            z: xz.z,
-        }
     }
 }
