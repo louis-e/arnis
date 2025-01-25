65cd0d453a3ad7e32739b36a1532e4b164392385
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-01-24 23:09:50 -0500

    element_processing/tree: replace round{1,2,3} with generic round
    round takes a parameter which is a reference to an array of patterns.
    The patterns are defined at the top of the file as `[(i32, i32, i32)]`.

--- src/element_processing/tree.rs
+++ src/element_processing/tree.rs
@@ -1,48 +1,55 @@
 use crate::block_definitions::*;
 use crate::world_editor::WorldEditor;
 
-/// Helper function to set blocks in a circular pattern around a central point.
-fn round1(editor: &mut WorldEditor, material: Block, x: i32, y: i32, z: i32) {
-    editor.set_block(material, x - 2, y, z, None, None);
-    editor.set_block(material, x + 2, y, z, None, None);
-    editor.set_block(material, x, y, z - 2, None, None);
-    editor.set_block(material, x, y, z + 2, None, None);
-    editor.set_block(material, x - 1, y, z - 1, None, None);
-    editor.set_block(material, x + 1, y, z + 1, None, None);
-    editor.set_block(material, x + 1, y, z - 1, None, None);
-    editor.set_block(material, x - 1, y, z + 1, None, None);
-}
+/// A circular pattern around a central point.
+const ROUND1_COORDS: [(i32, i32, i32); 8] = [
+    (-2, 0, 0),
+    (2, 0, 0),
+    (0, 0, -2),
+    (0, 0, 2),
+    (-1, 0, -1),
+    (1, 0, 1),
+    (1, 0, -1),
+    (-1, 0, 1),
+];
 
-/// Helper function to set blocks in a wider circular pattern.
-fn round2(editor: &mut WorldEditor, material: Block, x: i32, y: i32, z: i32) {
-    editor.set_block(material, x + 3, y, z, None, None);
-    editor.set_block(material, x + 2, y, z - 1, None, None);
-    editor.set_block(material, x + 2, y, z + 1, None, None);
-    editor.set_block(material, x + 1, y, z - 2, None, None);
-    editor.set_block(material, x + 1, y, z + 2, None, None);
-    editor.set_block(material, x - 3, y, z, None, None);
-    editor.set_block(material, x - 2, y, z - 1, None, None);
-    editor.set_block(material, x - 2, y, z + 1, None, None);
-    editor.set_block(material, x - 1, y, z + 2, None, None);
-    editor.set_block(material, x - 1, y, z - 2, None, None);
-    editor.set_block(material, x, y, z - 3, None, None);
-    editor.set_block(material, x, y, z + 3, None, None);
-}
+/// A wider circular pattern.
+const ROUND2_COORDS: [(i32, i32, i32); 12] = [
+    (3, 0, 0),
+    (2, 0, -1),
+    (2, 0, 1),
+    (1, 0, -2),
+    (1, 0, 2),
+    (-3, 0, 0),
+    (-2, 0, -1),
+    (-2, 0, 1),
+    (-1, 0, 2),
+    (-1, 0, -2),
+    (0, 0, -3),
+    (0, 0, 3),
+];
 
-/// Helper function to set blocks in a more scattered circular pattern.
-fn round3(editor: &mut WorldEditor, material: Block, x: i32, y: i32, z: i32) {
-    editor.set_block(material, x + 3, y, z - 1, None, None);
-    editor.set_block(material, x + 3, y, z + 1, None, None);
-    editor.set_block(material, x + 2, y, z - 2, None, None);
-    editor.set_block(material, x + 2, y, z + 2, None, None);
-    editor.set_block(material, x + 1, y, z - 3, None, None);
-    editor.set_block(material, x + 1, y, z + 3, None, None);
-    editor.set_block(material, x - 3, y, z - 1, None, None);
-    editor.set_block(material, x - 3, y, z + 1, None, None);
-    editor.set_block(material, x - 2, y, z - 2, None, None);
-    editor.set_block(material, x - 2, y, z + 2, None, None);
-    editor.set_block(material, x - 1, y, z + 3, None, None);
-    editor.set_block(material, x - 1, y, z - 3, None, None);
+/// A more scattered circular pattern.
+const ROUND3_COORDS: [(i32, i32, i32); 12] = [
+    (3, 0, -1),
+    (3, 0, 1),
+    (2, 0, -2),
+    (2, 0, 2),
+    (1, 0, -3),
+    (1, 0, 3),
+    (-3, 0, -1),
+    (-3, 0, 1),
+    (-2, 0, -2),
+    (-2, 0, 2),
+    (-1, 0, 3),
+    (-1, 0, -3),
+];
+
+/// Helper function to set blocks in various patterns.
+fn round(editor: &mut WorldEditor, material: Block, x: i32, y: i32, z: i32, block_pattern: &[(i32, i32, i32)]) {
+    for (i, j, k) in block_pattern {
+        editor.set_block(material, x + i, y + j, z + k, None, None);
+    }
 }
 
 /// Function to create different types of trees.
@@ -61,41 +68,51 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
         1 => {
             // Oak tree
             editor.fill_blocks(OAK_LOG, x, y, z, x, y + 8, z, None, None);
-            editor.fill_blocks(OAK_LEAVES, x - 1, y + 3, z, x - 1, y + 9, z, None, None);
-            editor.fill_blocks(OAK_LEAVES, x + 1, y + 3, z, x + 1, y + 9, z, None, None);
-            editor.fill_blocks(OAK_LEAVES, x, y + 3, z - 1, x, y + 9, z - 1, None, None);
-            editor.fill_blocks(OAK_LEAVES, x, y + 3, z + 1, x, y + 9, z + 1, None, None);
-            editor.fill_blocks(OAK_LEAVES, x, y + 9, z, x, y + 10, z, None, None);
+            let leaves_fill_coords = [
+                ((-1, 3, 0), (-1, 9, 0)),
+                ((1, 3, 0), (1, 9, 0)),
+                ((0, 3, -1), (0, 9, -1)),
+                ((0, 3, 1), (0, 9, 1)),
+                ((0, 9, 0), (0, 10, 0)),
+            ];
+            for ((x1, y1, z1), (x2, y2, z2)) in leaves_fill_coords {
+                editor.fill_blocks(OAK_LEAVES, x1, y1, z1, x2, y2, z2, None, None);
+            }
 
             for i in (3..=8).rev() {
-                round1(editor, OAK_LEAVES, x, y + i, z);
+                round(editor, OAK_LEAVES, x, y + i, z, &ROUND1_COORDS);
             }
 
             for i in (4..=7).rev() {
-                round2(editor, OAK_LEAVES, x, y + i, z);
+                round(editor, OAK_LEAVES, x, y + i, z, &ROUND2_COORDS);
             }
 
             for i in (5..=6).rev() {
-                round3(editor, OAK_LEAVES, x, y + i, z);
+                round(editor, OAK_LEAVES, x, y + i, z, &ROUND3_COORDS);
             }
 
             if snow {
-                editor.set_block(SNOW_LAYER, x, y + 11, z, None, None);
-                editor.set_block(SNOW_LAYER, x + 1, y + 10, z, None, None);
-                editor.set_block(SNOW_LAYER, x - 1, y + 10, z, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 10, z - 1, None, None);
-                editor.set_block(SNOW_LAYER, x, y + 10, z + 1, None, None);
+                let snow_coords = [
+                    (0, 11, 0), 
+                    (1, 10, 0),
+                    (-1, 10, 0),
+                    (0, 10, -1),
+                    (0, 10, 1),
+                ];
+                for (x, y, z) in snow_coords {
+                    editor.set_block(SNOW_LAYER, x, y, z, None, None);
+                }
 
                 for i in (6..=9).rev() {
-                    round1(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
                 }
 
                 for i in (5..=8).rev() {
-                    round2(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
                 }
 
                 for i in (6..=7).rev() {
-                    round3(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND3_COORDS);
                 }
             }
         }
@@ -109,11 +126,11 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
             editor.set_block(BIRCH_LEAVES, x, y + 10, z, None, None);
 
             for i in [9, 7, 6, 4, 3] {
-                round1(editor, BIRCH_LEAVES, x, y + i, z);
+                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
             }
 
             for i in [6, 3] {
-                round2(editor, BIRCH_LEAVES, x, y + i, z);
+                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
             }
 
             if snow {
@@ -124,11 +141,11 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                 editor.set_block(SNOW_LAYER, x, y + 11, z + 1, None, None);
 
                 for i in [10, 8, 7, 5, 4] {
-                    round1(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
                 }
 
                 for i in [7, 4] {
-                    round2(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
                 }
             }
         }
@@ -142,11 +159,11 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
             editor.fill_blocks(BIRCH_LEAVES, x, y + 7, z, x, y + 8, z, None, None);
 
             for i in (2..=6).rev() {
-                round1(editor, BIRCH_LEAVES, x, y + i, z);
+                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND1_COORDS);
             }
 
             for i in 2..=4 {
-                round2(editor, BIRCH_LEAVES, x, y + i, z);
+                round(editor, BIRCH_LEAVES, x, y + i, z, &ROUND2_COORDS);
             }
 
             if snow {
@@ -157,11 +174,11 @@ pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u
                 editor.set_block(SNOW_LAYER, x, y + 8, z + 1, None, None);
 
                 for i in (3..=7).rev() {
-                    round1(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND1_COORDS);
                 }
 
                 for i in 3..=5 {
-                    round2(editor, SNOW_LAYER, x, y + i, z);
+                    round(editor, SNOW_LAYER, x, y + i, z, &ROUND2_COORDS);
                 }
             }
         }
