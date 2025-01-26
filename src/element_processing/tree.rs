9b46bc8d76b5f13aa07d345c35eb56726d0e6641
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-01-25 23:20:09 -0500

    Change functions returning vecs to global arrays.
    I'm not sure why these were initally functions. I think they are better
    off as global arrays, since the function would initialize a new vec
    every time its called, whereas this only initializes it once.

--- src/element_processing/tree.rs
+++ src/element_processing/tree.rs
@@ -48,7 +48,7 @@ fn round3(editor: &mut WorldEditor, material: Block, x: i32, y: i32, z: i32) {
 /// Function to create different types of trees.
 pub fn create_tree(editor: &mut WorldEditor, x: i32, y: i32, z: i32, typetree: u8, snow: bool) {
     let mut blacklist: Vec<Block> = Vec::new();
-    blacklist.extend(building_corner_variations());
+    blacklist.extend(BUILDING_CORNER_VARIATIONS);
     blacklist.extend(building_wall_variations());
     blacklist.extend(building_floor_variations());
     blacklist.push(WATER);
