5c0e24ee73fdbbb98d96ac01b419af97015df6b4
Oleg4260 <oleg.kucheryavenko2018@gmail.com>
2025-04-06 18:29:35 +0300

    fix fmt

--- src/element_processing/landuse.rs
+++ src/element_processing/landuse.rs
@@ -197,8 +197,8 @@ pub fn generate_landuse(
             "farmland" => {
                 // Check if the current block is not water or another undesired block
                 if !editor.check_for_block(x, ground_level, z, None, Some(&[WATER, ICE])) {
-                    if x % 8 == 0 && z % 8 == 0 { // Dot pattern
-                        // Place water/ice
+                    if x % 8 == 0 && z % 8 == 0 {
+                        // Place water/ice in dot pattern
                         editor.set_block(
                             if args.winter { ICE } else { WATER },
                             x,
