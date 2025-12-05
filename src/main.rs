7a109cce0b5e505e92e1dffe0a7c8ee082ad296c
louis-e <44675238+louis-e@users.noreply.github.com>
2025-12-05 16:51:30 +0100

    Mock emit_open_mcworld_file

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ mod progress {
     pub fn emit_gui_error(_message: &str) {}
     pub fn emit_gui_progress_update(_progress: f64, _message: &str) {}
     pub fn emit_map_preview_ready() {}
+    pub fn emit_open_mcworld_file(_path: &str) {}
     pub fn is_running_with_gui() -> bool {
         false
     }
