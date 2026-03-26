e474389f47109e9d1f8e882b01279f0a4766a97c
Louis Erbkamm <info@louisdev.de>
2026-03-27 00:59:04 +0100

    Remove comment

--- src/element_processing/leisure.rs
+++ src/element_processing/leisure.rs
@@ -33,8 +33,8 @@ pub fn generate_leisure(
                         "sand" => SAND,
                         "tartan" => RED_TERRACOTTA,
                         "grass" => GRASS_BLOCK,
-                        "dirt" | "ground" | "earth" => DIRT,           //Added
-                        "mulch" => PODZOL,                             // Added
+                        "dirt" | "ground" | "earth" => DIRT,
+                        "mulch" => PODZOL,
                         "pebblestone" | "cobblestone" | "unhewn_cobblestone" => COBBLESTONE,
                         _ => GREEN_STAINED_HARDENED_CLAY,
                     }
